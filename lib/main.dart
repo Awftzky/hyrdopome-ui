13f06c549910cbbbc8fd209d38fa2788af2518fa
Awftzky <Aufaazzakyarief@gmail.com>
2025-11-04 17:55:29 +0700

    setup project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lollipopteam/App/feature/splashscreen&onboarding/view/splashscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/*
+App dirunning di setiap page , untuk fungsionalitas seperti routing belum diajarkan dan 
+digunakan , notes : SCAFFOLD disetiap page & void main disetiap class feature
+*/
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Splashscreen());
+  }
+}
