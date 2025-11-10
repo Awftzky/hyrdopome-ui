c9ec76f5e7a8fad5f661061eac2e5e1fe5b998fc
Awftzky <Aufaazzakyarief@gmail.com>
2025-11-10 15:46:47 +0700

    Splashscreen view

--- lib/App/feature/splashscreen&onboarding/view/splashscreen_view.dart
+++ lib/App/feature/splashscreen&onboarding/view/splashscreen_view.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const Splashscreen());
+}
+
 class Splashscreen extends StatelessWidget {
   const Splashscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'PlusJakartaSans'),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets//images/logo.png', width: 98, height: 104),
+              const SizedBox(height: 20),
+              const Text(
+                "HydropoMe",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF179778),
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text(
+                "Hijaukan bumi dengan inovasi",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xFF179778),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
